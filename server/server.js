47d0ae67feb8f1f44d47b25dfa665e5cdc7d78d1
Meenakshi <meenakshi.v36@gmail.com>
2026-01-07 12:56:36 +0530

    feat: updated server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import taskRoutes from "./routes/taskRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -13,6 +14,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use("/api/tasks", taskRoutes);
 
 // Test route
 app.get("/", (req, res) => {
