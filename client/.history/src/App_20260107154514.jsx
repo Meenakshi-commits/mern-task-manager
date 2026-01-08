e4c19a1917a22f9ff8034bc3bc7f0567b636e026
Meenakshi <meenakshi.v36@gmail.com>
2026-01-08 11:41:08 +0530

    feat: setup react frontend and connect to tasks API

---
+++ client/.history/src/App_20260107154514.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Task Manager</h1>
+      <p>Frontend is running 🚀</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/.history/src/api_20260108113543.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:5000/api/tasks";
+
+export const getTasks = () => axios.get(API_URL);
+export const getTaskCount = () => axios.get(`${API_URL}/count`);
